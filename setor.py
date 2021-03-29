1a6bd3e5f8379e19f8778e7547d3606485e0ba73
vitor-o-s <vitorsantos.2018@alunos.utfpr.edu.br>
2021-03-29 15:05:20 -0300

    update setor

--- setor.py
+++ setor.py
@@ -1,8 +1,20 @@
-class Setor():
+import tkinter as tk
+from tkinter import Label, Entry
 
-    def __init__(self, sector_code, name, coordinator_name):
-        self.sector_code = sector_code
-        self.name = name
-        self.coordinator_name = coordinator_name
+def CadastroSetor():
 
-    
+    Setor = tk.Tk()
+    Setor.geometry('400x300')
+    Setor.title('Cadastro de Setores')
+    lblCodigo = Label(Setor, text='Codigo:')
+    lblCodigo.grid(column=0, row=0)
+    txtCodigo = Entry(Setor, width=10)
+    txtCodigo.grid(column=1, row=0)
+    lblNomeSetor = Label(Setor, text='Setor:')
+    lblNomeSetor.grid(column=0, row=1)
+    txtNomeSetor = Entry(Setor, width=30)
+    txtNomeSetor.grid(column=1, row=1)
+    lblCodigoCoordenador = Label(Setor, text='Nome Coordenador:')
+    lblCodigoCoordenador.grid(column=0, row=2)
+    txtCodigoCoordenador = Entry(Setor, width=10)
+    txtCodigoCoordenador.grid(column=1, row=2)
