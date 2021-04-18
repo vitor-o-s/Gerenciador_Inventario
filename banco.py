2e669e4e1ca8d57647e974c16e6b705d412db556
Matheusfy <matheusy@alunos.utfpr.edu.br>
2021-04-18 01:01:03 -0300

    Inserção da função do botão com o banco

--- banco.py
+++ banco.py
@@ -1,16 +1,37 @@
 import psycopg2
+pswd = 'e08769fa9d15c60f3f5c20446c21e804c7242e853e06a05e1d21068a607a379f'
+host = 'ec2-34-233-0-64.compute-1.amazonaws.com'
+user = 'ozuqxrhmnofbkl'
+db   = 'd3v409sdpeglku'
 
 
-def salvarpessoa(codigo, nome, cargo, email):
+def salvarpessoa(nome, cargo, email):
+    
+    conn = psycopg2.connect(host=host,database=db, user=user, password=pswd)
+    
+    try:
+        if conn is not None:
+            print('Connection established to PostgreSQL.')
+            with conn.cursor() as cur:
+                #Example
+                # cur.execute("""
+                #             INSERT INTO PESSOA (nomeCompleto, email)
+                #             VALUES (%s, %s, %s);
+                #             """,
+                #             (str(nome), str(email)))
 
-    with psycopg2.connect(host="Localhost",database="mydb", user="postgres", password="123") as conn:
+                cur.execute("SELECT * FROM PESSOA")
+                cur.fetchone()            
+            conn.commit()
+        else:
+            print('Connection not established to PostgreSQL.')
+            
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Finally, connection closed.')
 
-        with conn.cursor() as cur:
-            #Example
-            cur.execute("""
-                        INSERT INTO PESSOA (codPessoa, nomeCompleto, email)
-                        VALUES (%s, %s, %s);
-                        """,
-                        (int(codigo),str(nome), str(email)))
-                        
-        conn.commit()
+
