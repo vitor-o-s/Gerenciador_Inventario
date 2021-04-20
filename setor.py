4234572c222c6ef7f0b7e9d6a7b212aefba104e0
vitor-o-s <vitorsantos.2018@alunos.utfpr.edu.br>
2021-04-20 13:52:55 -0300

    add libs add checkfill add btn/lbl result

--- setor.py
+++ setor.py
@@ -1,5 +1,8 @@
+import banco
 import tkinter as tk
-from tkinter import Label, Entry
+from tkinter import Label, Entry, Button
+from functools import partial
+
 
 def CadastroSetor():
 
@@ -18,3 +21,18 @@ def CadastroSetor():
     lblCodigoCoordenador.grid(column=0, row=2)
     txtCodigoCoordenador = Entry(Setor, width=10)
     txtCodigoCoordenador.grid(column=1, row=2)
+
+
+    labelResult = tk.Label(Setor)  
+    labelResult.grid(row=4, column=1) 
+
+    btnIncluir = Button(Setor, text='Incluir', command = partial())
+    btnIncluir.grid(column=1, row=3)
+
+def FuncaoButton():
+
+    pass
+
+def checkfill(codigo, setor, nomecoord):
+
+    return codigo.get()=='' or nomecoord.get()=='' or setor.get==''
