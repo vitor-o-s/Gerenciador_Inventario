e0463f10c9a7d7307c9d2a4bbd99d584f025b4a8
vitor-o-s <vitorsantos.2018@alunos.utfpr.edu.br>
2021-04-20 13:33:59 -0300

    validacacoes entradas e formatacao do cod

--- pessoa.py
+++ pessoa.py
@@ -6,48 +6,61 @@ import banco
 def CadastroPessoa():
     Pessoa = tk.Tk()
     
-    Pessoa.geometry('400x300') #tamanho tela
-    Pessoa.title("Cadastro de Pessoas") # titulo
-    # lblCodigo = Label(Pessoa, text='Código:') 
-    # lblCodigo.grid(column=0, row=0)
-    # txtCodigo = Entry(Pessoa, width=10)
-    # txtCodigo.grid(column=1, row=0)
+    Pessoa.geometry('400x300')
+    Pessoa.title("Cadastro de Pessoas")
     lblNomePessoa = Label(Pessoa, text='Nome:')
     lblNomePessoa.grid(column=0, row=1)
     txtNomePessoa = Entry(Pessoa, width=50)
     txtNomePessoa.grid(column=1, row=1)
-    lblCargo = Label(Pessoa, text='Cargo:')
-    lblCargo.grid(column=0, row=2)
-    txtCargo = Entry(Pessoa, width=50)
-    txtCargo.grid(column=1, row=2)
     lblEmail = Label(Pessoa, text='E-mail:')
     lblEmail.grid(column=0, row=3)
     txtEmail = Entry(Pessoa, width=50)
     txtEmail.grid(column=1, row=3)
+    '''
+	nomeCompleto VARCHAR(70) - ja tem
+	dataNascimento date, - sem por enquanto
+	email VARCHAR(50), - ja tem 
+	codSetor INTEGER, - lista de setores disponiveis?
+	macETH MACADDR, - sem preenchimento aqui?
+	macWLAN MACADDR - sem preenchimento aqui?
+
+    '''
     
     labelResult = tk.Label(Pessoa)  
     labelResult.grid(row=7, column=1) 
 
-    btnIncluir = Button(Pessoa, text='Incluir', command = partial(FuncaoButton, txtNomePessoa, txtCargo, txtEmail,labelResult))
+    btnIncluir = Button(Pessoa, text='Incluir', command = partial(FuncaoButton, txtNomePessoa, txtEmail,labelResult))
     btnIncluir.grid(column=1, row=6)
 
 
 
 
-def FuncaoButton(nome, cargo, email,labelResult):
+def FuncaoButton(nome, email,labelResult):
 
-    if ((nome.get() == '') or (cargo.get() == '' ) or (email.get() == ''))  :
-        print(" Nome ou Cargo ou Email não foi preenchido. Favor verificar")
-        labelResult.config(text="Nome ou Cargo ou Email não foi preenchido. Favor verificar") 
+    if checkfill(nome,email):
+        labelResult.config(text="Nome ou Cargo ou Email não foi preenchido. Favor verificar")
+
     else:
-        resultado = banco.salvarpessoa(nome.get(), cargo.get(), email.get(), labelResult)
-        if resultado == 1:
-            labelResult.config(text="Usuario cadastrado com sucesso")
-        elif resultado == -1:
-            labelResult.config(text="Dominio inválido")
-        elif resultado == 0:
-            labelResult.config(text="Usuário já existe na tabela")
+        if mask(email.get())==1:
+            resultado = banco.salvarpessoa(nome.get(), email.get(), labelResult)
+            if resultado == 1:
+                labelResult.config(text="Usuario cadastrado com sucesso")
+            elif resultado == -1:
+                labelResult.config(text="Dominio inválido")
+            elif resultado == 0:
+                labelResult.config(text="Usuário já existe na tabela")
+        else:
+            print("Dominio email invalido")
+            return -1
 
+def checkfill(nome, email):
 
-    
-    
+    return nome.get()=='' or email.get()=='' 
+
+def mask(s):
+    lo = s.find('@')
+    return checkDomain(s, lo) == 1
+
+def checkDomain(s, lo):
+    dominio_email = s[lo:]
+    return dominio_email == '@empresa.com.br'
